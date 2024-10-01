743a02dea9f5c75890dfe4d69798b28fd88fd696
unknown <danbuitrago87@gmail.com>
2024-09-30 20:06:33 -0500

    CastingJava corregidas entradas

--- src/CastingJava.java
+++ src/CastingJava.java
@@ -4,15 +4,15 @@ public class CastingJava {
     public static void main (String[] args){
         //CASTING DE VARIABLES
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Digite el valor que desea transformar: ");
+        System.out.println("Digite un valor o cadena de entrada: ");
         String entrada= scanner.nextLine();
         try {
             int valorEntero = Integer.parseInt(entrada);
-            System.out.println("El valor digitado es entero: " + valorEntero);
+            System.out.println("La entrada es entero: " + valorEntero);
             } catch(NumberFormatException e1){
                 try {
                     double valorDouble = Double.parseDouble(entrada);
-                    System.out.println("El valor digitado es decimal: " + valorDouble);
+                    System.out.println("La entrada es decimal: " + valorDouble);
                 }catch(NumberFormatException e2){
                         System.out.println("La entrada digitada es Cadena: " + entrada);
                     }
